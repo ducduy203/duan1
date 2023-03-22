2abd421caa8e9bffd457fa5acbe5f20178904b69
thaoo153 <ngocthaopham1503@gmail.com>
2023-03-22 22:56:14 +0700

    folder admin

---
+++ admin/index.php
@@ -0,0 +1,12 @@
+<html>
+
+<head>
+    <title></title>
+    <link rel="stylesheet" href="../css/admin.css">
+</head>
+
+<body>
+
+</body>
+
+</html>
---
+++ admin/manage-admin.php
@@ -0,0 +1,23 @@
+<div class="main-content">
+    <div class="wrapper">
+        <h1>Manage Admin</h1>
+
+        <table>
+            <tr>
+                <th>NO.</th>
+                <th>Full Name</th>
+                <th>User Name</th>
+                <th>Actions</th>
+            </tr>
+            <tr>
+                <td></td>
+                <td>đa</td>
+                <td>đấ</td>
+                <td>
+                    Update Admin
+                    Delete Admin
+                </td>
+            </tr>
+        </table>
+    </div>
+</div>
