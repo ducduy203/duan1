136bbe4a6089650113ccff5f77f92f5ffbcf866a
duyndph43896 <duyndph43896@fpt.edu.vn>
2023-04-06 21:40:57 +0700

    header

--- views/home.php
+++ views/home.php
@@ -1,4 +1,4 @@
-<div id="carouselExampleRide" class="container carousel slide px-3" data-bs-ride="true">
+<div id="carouselExampleAutoplaying" class="container carousel slide px-3" data-bs-ride="carousel">
     <div class="carousel-inner">
         <div class="carousel-item active">
         <img src="views/images/banner1.jpg" class="d-block w-100" style="height: 400px" alt="">
@@ -10,11 +10,11 @@
         <img src="views/images/banner3.jpg" class="d-block w-100" style="height: 400px" alt="">
         </div>
     </div>
-    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleRide" data-bs-slide="prev">
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
         <span class="visually-hidden">Previous</span>
     </button>
-    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleRide" data-bs-slide="next">
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
         <span class="carousel-control-next-icon" aria-hidden="true"></span>
         <span class="visually-hidden">Next</span>
     </button>
