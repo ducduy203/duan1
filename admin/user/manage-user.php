fac04ba26b820fa1aebe694cbde24468b1539cd1
Ngokhanh16 <ngoduykhanhnd2003@gmail.com>
2023-04-07 21:58:30 +0700

    Khanh

--- admin/user/manage-user.php
+++ admin/user/manage-user.php
@@ -1,15 +1,15 @@
 <div class="main-content">
     <div class="wrapper">
         <h1>Manage User</h1>
-        <a class="btn btn-primary mb-4" href="user/add-user.php">Add user</a>
-
         <table class="table">
             <thead>
                 <tr>
                     <th class="table-danger" scope="col"></th>
                     <th class="table-danger" scope="col">ID</th>
-                    <th class="table-danger" scope="col">Full Name</th>
+                    <th class="table-danger" scope="col">Email</th>
                     <th class="table-danger" scope="col">Username</th>
+                    <th class="table-danger" scope="col">Address</th>
+                    <th class="table-danger" scope="col">Tel</th>
                     <th class="table-danger" scope="col">Action</th>
                 </tr>
             </thead>
@@ -21,8 +21,10 @@
                     echo '<tr>
                         <td><input type="checkbox" name="" id=""></td>
                         <td>' . $id . '</td>
-                        <td>' . $full_name . '</td>
+                        <td>' . $email . '</td>
                         <td>' . $username . '</td>
+                        <td>' . $address . '</td>
+                        <td>' . $tel . '</td>
                         <td>
                             <a href="' . $deleteuser . '">
                                 <input class="btn btn-danger" onclick="return confirm(`Are you sure?`);" type="button" value="Delete">
